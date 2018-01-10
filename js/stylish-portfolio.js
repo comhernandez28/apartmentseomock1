e580720ab309e7d4d512331f4ccc25bbd771fc2f
comhernandez28 <comhernandez28@gmail.com>
2018-01-10 11:40:46 -0800

    Major review

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -5,58 +5,60 @@
  */
 
 (function($) {
-    "use strict"; // Start of use strict
+  "use strict"; // Start of use strict
 
-    // Closes the sidebar menu
-    $("#menu-close, .sidebar-nav>li>a").click(function(e) {
-        //e.preventDefault();
-        $("#sidebar-wrapper").toggleClass("active");
-    });
+  // Closes the sidebar menu
+  $("#menu-close, .sidebar-nav>li>a").click(function(e) {
+    //e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+    $("#sidebar-wrapper-curved").toggleClass("active");
 
-    // Opens the sidebar menu
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#sidebar-wrapper").toggleClass("active");
-    });
+  });
 
-    // Smooth scrolling using jQuery easing
-    $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html, body').animate({
-                    scrollTop: target.offset().top
-                }, 1000, "easeInOutExpo");
-                return false;
-            }
-        }
-    });
+  // Opens the sidebar menu
+  $("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+  });
 
-    //#to-top button appears after scrolling
-    var fixed = false;
-    $(document).scroll(function() {
-        if ($(this).scrollTop() > 250) {
-            if (!fixed) {
-                fixed = true;
-                $('#to-top').show("slow", function() {
-                    $('#to-top').css({
-                        position: 'fixed',
-                        display: 'block'
-                    });
-                });
-            }
-        } else {
-            if (fixed) {
-                fixed = false;
-                $('#to-top').hide("slow", function() {
-                    $('#to-top').css({
-                        display: 'none'
-                    });
-                });
-            }
-        }
-    });
+  // Smooth scrolling using jQuery easing
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  //#to-top button appears after scrolling
+  var fixed = false;
+  $(document).scroll(function() {
+    if ($(this).scrollTop() > 250) {
+      if (!fixed) {
+        fixed = true;
+        $('#to-top').show("slow", function() {
+          $('#to-top').css({
+            position: 'fixed',
+            display: 'block'
+          });
+        });
+      }
+    } else {
+      if (fixed) {
+        fixed = false;
+        $('#to-top').hide("slow", function() {
+          $('#to-top').css({
+            display: 'none'
+          });
+        });
+      }
+    }
+  });
 
 })(jQuery); // End of use strict
 
@@ -64,29 +66,29 @@
 // See http://stackoverflow.com/a/25904582/1607849
 // Disable scroll zooming and bind back the click event
 var onMapMouseleaveHandler = function(event) {
-    var that = $(this);
-    that.on('click', onMapClickHandler);
-    that.off('mouseleave', onMapMouseleaveHandler);
-    that.find('iframe').css("pointer-events", "none");
+  var that = $(this);
+  that.on('click', onMapClickHandler);
+  that.off('mouseleave', onMapMouseleaveHandler);
+  that.find('iframe').css("pointer-events", "none");
 }
 var onMapClickHandler = function(event) {
-        var that = $(this);
-        // Disable the click handler until the user leaves the map area
-        that.off('click', onMapClickHandler);
-        // Enable scrolling zoom
-        that.find('iframe').css("pointer-events", "auto");
-        // Handle the mouse leave event
-        that.on('mouseleave', onMapMouseleaveHandler);
-    }
-    // Enable map zooming with mouse scroll when the user clicks the map
+  var that = $(this);
+  // Disable the click handler until the user leaves the map area
+  that.off('click', onMapClickHandler);
+  // Enable scrolling zoom
+  that.find('iframe').css("pointer-events", "auto");
+  // Handle the mouse leave event
+  that.on('mouseleave', onMapMouseleaveHandler);
+}
+// Enable map zooming with mouse scroll when the user clicks the map
 $('.map').on('click', onMapClickHandler);
 
 
 //onload
-function slideIn(){
+function slideIn() {
 
   //$('.slide').on('click', function(){
-      $('.holder').addClass('show');
+  $('.holder').addClass('show');
   //});
 
 };
@@ -104,13 +106,13 @@ $('.carousel img').each(function() {
   var $src = $(this).attr('src');
   var $color = $(this).attr('data-color');
   $(this).parent().css({
-    'background-image' : 'url(' + $src + ')',
-    'background-color' : $color
+    'background-image': 'url(' + $src + ')',
+    'background-color': $color
   });
   $(this).remove();
 });
 
-$(window).on('resize', function (){
+$(window).on('resize', function() {
   $wHeight = $(window).height();
   $item.height($wHeight);
 });
@@ -123,88 +125,98 @@ $('.carousel').carousel({
 //form
 
 /* Float Label Pattern Plugin for Bootstrap 3.1.0 by Travis Wilson
-**************************************************/
+ **************************************************/
 
-(function ($) {
-    $.fn.floatLabels = function (options) {
+(function($) {
+  $.fn.floatLabels = function(options) {
 
-        // Settings
-        var self = this;
-        var settings = $.extend({}, options);
+    // Settings
+    var self = this;
+    var settings = $.extend({}, options);
 
 
-        // Event Handlers
-        function registerEventHandlers() {
-            self.on('input keyup change', 'input, textarea', function () {
-                actions.swapLabels(this);
-            });
+    // Event Handlers
+    function registerEventHandlers() {
+      self.on('input keyup change', 'input, textarea', function() {
+        actions.swapLabels(this);
+      });
+    }
+
+
+    // Actions
+    var actions = {
+      initialize: function() {
+        self.each(function() {
+          var $this = $(this);
+          var $label = $this.children('label');
+          var $field = $this.find('input,textarea').first();
+
+          if ($this.children().first().is('label')) {
+            $this.children().first().remove();
+            $this.append($label);
+          }
+
+          var placeholderText = ($field.attr('placeholder') && $field.attr('placeholder') != $label.text()) ? $field.attr('placeholder') : $label.text();
+
+          $label.data('placeholder-text', placeholderText);
+          $label.data('original-text', $label.text());
+
+          if ($field.val() == '') {
+            $field.addClass('empty')
+          }
+        });
+      },
+      swapLabels: function(field) {
+        var $field = $(field);
+        var $label = $(field).siblings('label').first();
+        var isEmpty = Boolean($field.val());
+
+        if (isEmpty) {
+          $field.removeClass('empty');
+          $label.text($label.data('original-text'));
+        } else {
+          $field.addClass('empty');
+          $label.text($label.data('placeholder-text'));
         }
+      }
+    }
 
 
-        // Actions
-        var actions = {
-            initialize: function() {
-                self.each(function () {
-                    var $this = $(this);
-                    var $label = $this.children('label');
-                    var $field = $this.find('input,textarea').first();
+    // Initialization
+    function init() {
+      registerEventHandlers();
 
-                    if ($this.children().first().is('label')) {
-                        $this.children().first().remove();
-                        $this.append($label);
-                    }
-
-                    var placeholderText = ($field.attr('placeholder') && $field.attr('placeholder') != $label.text()) ? $field.attr('placeholder') : $label.text();
-
-                    $label.data('placeholder-text', placeholderText);
-                    $label.data('original-text', $label.text());
-
-                    if ($field.val() == '') {
-                        $field.addClass('empty')
-                    }
-                });
-            },
-            swapLabels: function (field) {
-                var $field = $(field);
-                var $label = $(field).siblings('label').first();
-                var isEmpty = Boolean($field.val());
-
-                if (isEmpty) {
-                    $field.removeClass('empty');
-                    $label.text($label.data('original-text'));
-                }
-                else {
-                    $field.addClass('empty');
-                    $label.text($label.data('placeholder-text'));
-                }
-            }
-        }
+      actions.initialize();
+      self.each(function() {
+        actions.swapLabels($(this).find('input,textarea').first());
+      });
+    }
+    init();
 
 
-        // Initialization
-        function init() {
-            registerEventHandlers();
+    return this;
+  };
 
-            actions.initialize();
-            self.each(function () {
-                actions.swapLabels($(this).find('input,textarea').first());
-            });
-        }
-        init();
-
-
-        return this;
-    };
-
-    $(function () {
-        $('.float-label-control').floatLabels();
-    });
+  $(function() {
+    $('.float-label-control').floatLabels();
+  });
 })(jQuery);
 
 
 //social
 $("#share").jsSocials({
-    showLabel: false,
-    showCount: "inside",
-    shares: ["twitter", "facebook", "googleplus", "linkedin"]
+  showLabel: false,
+  showCount: "inside",
+  shares: ["twitter", "facebook", "googleplus", "linkedin"]
 });
+
+
+//Share button
+
+
+const trigger = document.querySelector('.primary');
+const container = document.querySelector('.button-group');
+
+trigger.addEventListener('click', () => {
+  container.classList.toggle('active');
+})
